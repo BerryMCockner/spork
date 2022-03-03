bfe45e5bceb4336e5ddcfd8832920de839daaab5
BerryMCockner <100737280+BerryMCockner@users.noreply.github.com>
2022-03-03 14:18:11 -0700

    Update playsound.bat

--- playsound.bat
+++ playsound.bat
@@ -1,3 +1,1 @@
-powershell -Command "$PlayWav=New-Object System.Media.SoundPlayer"
-powershell -Command "$PlayWav.SoundLocation=($env:USERPROFILE + '/spork-main/windowsboot.wav')"
-powershell -Command "$PlayWav.playsync()"
+powershell -Command "$PlayWav=New-Object System.Media.SoundPlayer;$PlayWav.SoundLocation=($env:USERPROFILE + '/spork-main/windowsboot.wav');$PlayWav.playsync()"
